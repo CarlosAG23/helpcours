ed16503506457241b8fd51f20a8d8d2ee1a6dcb0
Carlos AG <carlosmrg4@gmail.com>
2022-06-04 16:45:12 -0500

    comits

--- correo.php
+++ correo.php
@@ -21,7 +21,7 @@ try {
     $mail->Host       = 'smtp.titan.email';       //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                   //Enable SMTP authentication
     $mail->Username   = 'info@helpcourse.xyz';       //SMTP username
-    $mail->Password   = '@Helpcours4';              //SMTP password
+    $mail->Password   = '';              //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;    //Enable implicit TLS encryption
     $mail->Port       = 465;            //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
