cae664ce0842fa78a7379833dc3564e91e224e46
CarlosWay23 <67843396+CarlosWay23@users.noreply.github.com>
2022-06-04 19:19:57 -0500

    Update dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "helpcour_admin", "@helpcours4", "helpcour_login_register_pure_coding");
+$con=mysqli_connect("", "", "", "");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
